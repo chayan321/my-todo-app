65bb5f235721eecfe6c3b9086d79f80e55309ab3
chayan das <chayan.das@publicissapient.com>
2022-12-15 12:29:55 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      Hello React
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello React
+//     </div>
+//   );
+// }
+class App extends Component {
+  render() {
+     return (
+      <div className="App">
+      Hello
+      </div>
+     );
+  }
 }
 
 export default App;
